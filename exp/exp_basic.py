506a2fd739853a3a93341658c27c3fc6ba45df51
zorodav <93968419+zorodav@users.noreply.github.com>
2025-04-21 21:45:35 -0700

    Update exp_basic.py

--- exp/exp_basic.py
+++ exp/exp_basic.py
@@ -7,23 +7,21 @@ class Exp_Basic(object):
     def __init__(self, args):
         self.args = args
         self.device = self._acquire_device()
-        self.model = self._build_model().to(self.device)
-
+        self.model = self._build_model()
     def _build_model(self):
         raise NotImplementedError
         return None
 
     def _acquire_device(self):
-        if self.args.use_gpu:
+        if self.args['use_gpu']:
             os.environ["CUDA_VISIBLE_DEVICES"] = str(
-                self.args.gpu) if not self.args.use_multi_gpu else self.args.devices
-            device = torch.device('cuda:{}'.format(self.args.gpu))
-            print('Use GPU: cuda:{}'.format(self.args.gpu))
+                self.args['gpu']) if not self.args['use_multi_gpu'] else self.args['devices']
+            device = torch.device('cuda:{}'.format(self.args['gpu']))
+            print('Use GPU: cuda:{}'.format(self.args['gpu']))
         else:
             device = torch.device('cpu')
             print('Use CPU')
         return device
-
     def _get_data(self):
         pass
 
