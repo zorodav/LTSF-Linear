5775dfd4dda49bb44ed26630e20f093baf955a9a
zorodav <93968419+zorodav@users.noreply.github.com>
2025-04-21 21:50:40 -0700

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -8,21 +8,21 @@ plt.switch_backend('agg')
 
 def adjust_learning_rate(optimizer, epoch, args):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
-    if args.lradj == 'type1':
-        lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
-    elif args.lradj == 'type2':
+    if args['lradj'] == 'type1':
+        lr_adjust = {epoch: args['learning_rate'] * (0.5 ** ((epoch - 1) // 1))}
+    elif args['lradj'] == 'type2':
         lr_adjust = {
             2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
             10: 5e-7, 15: 1e-7, 20: 5e-8
         }
-    elif args.lradj == '3':
-        lr_adjust = {epoch: args.learning_rate if epoch < 10 else args.learning_rate*0.1}
-    elif args.lradj == '4':
-        lr_adjust = {epoch: args.learning_rate if epoch < 15 else args.learning_rate*0.1}
-    elif args.lradj == '5':
-        lr_adjust = {epoch: args.learning_rate if epoch < 25 else args.learning_rate*0.1}
-    elif args.lradj == '6':
-        lr_adjust = {epoch: args.learning_rate if epoch < 5 else args.learning_rate*0.1}  
+    elif args['lradj'] == '3':
+        lr_adjust = {epoch: args['learning_rate'] if epoch < 10 else args['learning_rate']*0.1}
+    elif args['lradj'] == '4':
+        lr_adjust = {epoch: args['learning_rate'] if epoch < 15 else args['learning_rate']*0.1}
+    elif args['lradj'] == '5':
+        lr_adjust = {epoch: args['learning_rate'] if epoch < 25 else args['learning_rate']*0.1}
+    elif args['lradj'] == '6':
+        lr_adjust = {epoch: args['learning_rate'] if epoch < 5 else args['learning_rate']*0.1}  
     if epoch in lr_adjust.keys():
         lr = lr_adjust[epoch]
         for param_group in optimizer.param_groups:
@@ -37,7 +37,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
 
     def __call__(self, val_loss, model, path):
