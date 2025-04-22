a5788e16cfaeb0fb6917fde62a33a244fff129d4
zorodav <93968419+zorodav@users.noreply.github.com>
2025-04-21 21:54:06 -0700

    Update NLinear.py

--- models/NLinear.py
+++ models/NLinear.py
@@ -9,13 +9,13 @@ class Model(nn.Module):
     """
     def __init__(self, configs):
         super(Model, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
+        self.seq_len = configs['seq_len']
+        self.pred_len = configs['pred_len']
         
         # Use this line if you want to visualize the weights
         # self.Linear.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-        self.channels = configs.enc_in
-        self.individual = configs.individual
+        self.channels = configs['model']['parameters']['enc_in']
+        self.individual = configs['individual']
         if self.individual:
             self.Linear = nn.ModuleList()
             for i in range(self.channels):
@@ -28,7 +28,7 @@ class Model(nn.Module):
         seq_last = x[:,-1:,:].detach()
         x = x - seq_last
         if self.individual:
-            output = torch.zeros([x.size(0),self.pred_len,x.size(2)],dtype=x.dtype).to(x.device)
+            output = torch.zeros([x.size(0),self.pred_len,x.size(2)],dtype=x.dtype)
             for i in range(self.channels):
                 output[:,:,i] = self.Linear[i](x[:,:,i])
             x = output
